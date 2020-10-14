0fced84b5ca0671d1012938dbc7cb350339010f1
sumancfc <sumanstha999@gmail.com>
2020-10-14 16:02:40 +0545

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import store from "./store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
